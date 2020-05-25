3b413e7f93805f5eb402d6d53bf0bf7c6f9d17c6
Vasily Fedotov <vasilyfedotov12@gmail.com>
2020-05-24 22:52:26 -0400

    dashboard components initial styling

--- rocks.php
+++ rocks.php
@@ -9,29 +9,36 @@
 
     <link rel="stylesheet" href="styles/general.css">
     <link rel="stylesheet" href="styles/navigation.css">
+    <link rel="stylesheet" href="styles/dashboard.css">
 </head>
 <body>
     <?php include "common/header.php"?>
     <?php include "common/navigation.php"?>
     <main>
-        <ul>
-            <li>
+        <ul id="componentsContainer">
+            <li class="component">
                 <a href="images/rocks-1.jpeg">
-                    <img src="images/rocks-1.jpeg"
+                    <img class="componentImage" src="images/rocks-1.jpeg"
                          alt="You are strong">
                 </a>
+                <p class="componentLocation">Ravine Blvd, Oakwood MN</p>
+                <p class="componentDescription">You are Strong</p>
             </li>
-            <li>
+            <li class="component">
                 <a href="images/rocks-2.jpeg">
-                    <img src="images/rocks-2.jpeg"
+                    <img class="componentImage" src="images/rocks-2.jpeg"
                          alt="We remember the people who show up in our darkest hour">
                 </a>
+                <p class="componentLocation">Holland Dr, Oakwood MN</p>
+                <p class="componentDescription">Motivational rock</p>
             </li>
-            <li>
+            <li class="component">
                 <a href="images/rocks-3.jpeg">
-                    <img src="images/rocks-3.jpeg"
+                    <img class="componentImage" src="images/rocks-3.jpeg"
                          alt="You are not alone">
                 </a>
+                <p class="componentLocation">Burkley St, Port Russ MN</p>
+                <p class="componentDescription">You are not alone, a really long description that does not fit within the boundary and would eventually be cropped with ellipses</p>
             </li>
         </ul>
     </main>
